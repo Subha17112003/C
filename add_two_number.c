ed504bbad4f68ec934a1c40051b874884afe4642
Subha17112003 <subhagiri.6809@gmail.com>
2024-08-10 16:32:48 +0530

    gg

---
+++ add_two_number.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("Enter the first number:");
+    scanf("%d",&a);
+    printf("Enter your second number :");
+    scanf("%d",&b);
+    int sum =a+b;
+    printf("Sum of two numbers is : %d",sum);
+    return 0;
+}
